d4170cd5c991bfae69768f17a1d808125e677df3
Polina Vishnevskaya <polina@uni.minerva.edu>
2025-05-01 17:47:58 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-sql_parts = []
 
-for hour in range(1, 169):
-    part = f"""SUM(IF(
-        toStartOfHour(all_purchases.first_payment_date) = 
-        addHours(toStartOfHour(deduplicated_events.first_signup_time), {hour}),
-        1, 0)) AS paid_hour_{hour}"""
-    sql_parts.append(part)
-
-# Join all parts with commas and newlines
-full_sql = ",\n\n".join(sql_parts)
-
-print(full_sql)
